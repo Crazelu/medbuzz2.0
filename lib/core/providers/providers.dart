f1965429c61e46d5209c431b7b791351b5c5f01f
Gideon Peters <gideonpeters85@gmail.com>
2020-06-21 23:21:34 +0100

    fix: changed time picker widget, adjusted card elevations, refactored view and provider for schedule water reminder screen

--- lib/core/providers/providers.dart
+++ lib/core/providers/providers.dart
@@ -1,8 +1,10 @@
 import 'package:MedBuzz/ui/views/fitness_reminders/all_fitness_reminders_model.dart';
+import 'package:MedBuzz/ui/views/water_reminders/schedule_water_reminder_model.dart';
 import 'package:provider/provider.dart';
 
 //Add your view models in this widget
 
 final providers = <SingleChildCloneableWidget>[
-  ChangeNotifierProvider(create: (_) => FitnessSchedulesModel())
+  ChangeNotifierProvider(create: (_) => FitnessSchedulesModel()),
+  ChangeNotifierProvider(create: (_) => WaterReminderViewModel())
 ];
