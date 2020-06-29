0aca36b8560a8a089279f1950e3e461a54935a8e
Abere Fejiro <aberefejiro@gmail.com>
2020-06-29 22:25:14 +0100

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:MedBuzz/core/constants/route_names.dart';
 import 'package:MedBuzz/core/providers/providers.dart';
 import 'package:MedBuzz/ui/app_theme/app_theme.dart';
-<<<<<<< HEAD
 import 'package:MedBuzz/ui/views/Home.dart';
 import 'package:MedBuzz/ui/views/all_reminders/all_reminders_screen.dart';
 import 'package:MedBuzz/ui/views/fitness_reminders/all_fitness_reminders_screen.dart';
@@ -20,8 +19,6 @@ import 'package:MedBuzz/ui/views/signup_page/signup_screen.dart';
 import 'package:MedBuzz/ui/views/schedule-appointment/all_scheduled_appointment_reminders.dart';
 import 'package:MedBuzz/ui/views/password_recovery/forgot_password_reset.dart';
 
-=======
->>>>>>> c264dd08ba2d29b9f9f4e758b65d5e7db613119b
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -40,40 +37,10 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'MedBuzz',
         theme: appThemeLight,
-<<<<<<< HEAD
-        initialRoute: RouteNames.singleDietScreen,
-        routes: {
-          RouteNames.splashScreen: (context) => SplashScreen(),
-          RouteNames.home: (context) => HomeScreen(),
-          RouteNames.login: (context) => LoginPage(),
-          RouteNames.forgotPasswordSubmit: (context) => ForgotPasswordSubmit(),
-          RouteNames.profile: (context) => ProfilePage(),
-          RouteNames.onboarding: (context) => Onboard(),
-          RouteNames.homePage: (context) => HomePage(),
-          RouteNames.signup: (context) => Signup(),
-          RouteNames.allRemindersScreen: (context) => AllRemindersScreen(),
-          RouteNames.fitnessSchedulesScreen: (context) =>
-              FitnessSchedulesScreen(),
-          RouteNames.scheduleAppointmentScreen: (context) =>
-              ScheduleAppointmentScreen(),
-          RouteNames.scheduledAppointmentsPage: (context) =>
-              ScheduledAppointmentsPage(),
-          RouteNames.scheduleWaterReminderScreen: (context) =>
-              ScheduleWaterReminderScreen(),
-          RouteNames.waterScheduleView: (context) => WaterScheduleViewScreen(),
-          RouteNames.confirmOrSnoozeReminderScreen: (context) =>
-              ConfirmOrSnoozeScreen(),
-          RouteNames.forgotPasswordReset: (context) => ForgotPasswordReset(),
-          RouteNames.forgotPasswordMail: (context) => ForgotPasswordMail(),
-          RouteNames.singleDietScreen: (context) => SingleDiet(),
-        },
-
-=======
         initialRoute: RouteNames.splashScreen,
         //Crazelu moved the routes to RouteNames class to clean things up here
         //head over there if you need to add your named routes
         routes: RouteNames.routes,
->>>>>>> c264dd08ba2d29b9f9f4e758b65d5e7db613119b
       ),
     );
   }
