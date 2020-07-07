043fad1839a7297222394f0b4cd657f37801fba3
Rhemi7 <adelefatai@yahoo.com>
2020-07-08 00:03:14 +0100

    i pulled from upstream saw a lot of changes  so I had to work with those and rework my code

--- lib/ui/widget/medication_card.dart
+++ lib/ui/widget/medication_card.dart
@@ -1,227 +1,19 @@
 import 'package:MedBuzz/core/constants/route_names.dart';
-import 'package:MedBuzz/core/database/medication_data.dart';
-import 'package:MedBuzz/core/models/medication_reminder_model/medication_reminder.dart';
 import 'package:MedBuzz/ui/size_config/config.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-//class MedicationCard extends StatefulWidget {
-//  final double height;
-//  final double width;
-//  final String drugName;
-//  final String drugType;
-//  final String time;
-//  final int dosage;
-//  final String selectedFreq;
-//  final MedicationReminder medicationReminder;
-//  MedicationCard(
-//      {this.height,
-//      this.width,
-//      this.drugName,
-//      this.drugType,
-//      this.dosage,
-//      this.selectedFreq,
-//      this.time,
-//      this.medicationReminder});
-//  @override
-//  _MedicationCardState createState() => _MedicationCardState();
-//}
-//
-//class _MedicationCardState extends State<MedicationCard> {
-//  bool isSelected = false;
-//  @override
-//  Widget build(BuildContext context) {
-//    double height = MediaQuery.of(context).size.height;
-//    double width = MediaQuery.of(context).size.width;
-//    return GestureDetector(
-//      //Navigate to screen with single reminder i.e the on user clicked on
-//      onTap: () {
-//        setState(() => isSelected = !isSelected);
-//      },
-//
-//      child: Column(
-//          mainAxisAlignment: MainAxisAlignment.start,
-//          crossAxisAlignment: CrossAxisAlignment.start,
-//          mainAxisSize: MainAxisSize.max,
-//          children: [
-//            Text(
-//              widget.time,
-//            ),
-//            SizedBox(height: height * 0.02),
-//            Container(
-//                width: width,
-//                padding: EdgeInsets.symmetric(
-//                    horizontal: Config.xMargin(context, 3),
-//                    vertical: Config.yMargin(context, 1)),
-//                decoration: BoxDecoration(
-//                  color: isSelected
-//                      ? Theme.of(context).primaryColor
-//                      : Theme.of(context).primaryColorLight,
-//                  borderRadius:
-//                      BorderRadius.circular(Config.xMargin(context, 5)),
-//                  boxShadow: [
-//                    BoxShadow(
-//                      color: Theme.of(context).primaryColorLight,
-//                      spreadRadius: Config.xMargin(context, 2),
-//                    ),
-//                  ],
-//                ),
-//                child: Column(
-//                  children: <Widget>[
-//                    Row(
-//                      children: <Widget>[
-//                        Image.asset(
-//                          widget.drugType,
-////                            color: Theme.of(context).primaryColorLight,
-//                          width: width * 0.2,
-//                          height: height * 0.1,
-//                        ),
-//                        Column(
-//                          crossAxisAlignment: CrossAxisAlignment.start,
-//                          children: <Widget>[
-//                            Text(
-//                              widget.drugName,
-//                              style: TextStyle(
-//                                  color: isSelected
-//                                      ? Theme.of(context).primaryColorLight
-//                                      : Theme.of(context).primaryColorDark,
-//                                  fontWeight: FontWeight.bold),
-//                            ),
-//                            SizedBox(height: height * 0.005),
-//                            Text(
-//                              '${widget.dosage} - ${widget.selectedFreq} per day',
-//                              style: TextStyle(
-//                                  color: isSelected
-//                                      ? Theme.of(context).primaryColorLight
-//                                      : Theme.of(context).primaryColorDark),
-//                            ),
-//                          ],
-//                        ),
-//                      ],
-//                    ),
-//                    SizedBox(
-//                      height: Config.yMargin(context, 1),
-//                      width: double.infinity,
-//                    ),
-//                    Divider(
-//                      color: Theme.of(context).primaryColorLight,
-//                      height: height * 0.02,
-////indent: 50.0,
-//                      // endIndent: 10.0,
-//                    ),
-//                    Visibility(
-//                      visible: isSelected,
-//                      child: Row(
-//                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                        children: <Widget>[
-//                          FlatButton(
-//                            onPressed: () {
-//                              Navigator.pushNamed(
-//                                  context, RouteNames.medicationView);
-//                            },
-//                            child: Text(
-//                              'View',
-//                              style: TextStyle(
-//                                  fontWeight: FontWeight.bold,
-//                                  color: isSelected
-//                                      ? Theme.of(context).primaryColorLight
-//                                      : Theme.of(context).primaryColorDark),
-//                            ),
-//                          ),
-//                          FlatButton(
-//                            child: Row(
-//                              children: <Widget>[
-//                                Icon(
-//                                  Icons.close,
-//                                  color: isSelected
-//                                      ? Theme.of(context).primaryColorLight
-//                                      : Theme.of(context).primaryColorDark,
-//                                  size: Config.textSize(context, 3),
-//                                ),
-//                                SizedBox(
-//                                  width: Config.xMargin(context, 2),
-//                                ),
-//                                Text(
-//                                  'Skip',
-//                                  style: TextStyle(
-//                                      fontWeight: FontWeight.bold,
-//                                      color: isSelected
-//                                          ? Theme.of(context).primaryColorLight
-//                                          : Theme.of(context).primaryColorDark),
-//                                )
-//                              ],
-//                            ),
-//                            onPressed: () {},
-//                          ),
-//                          FlatButton(
-//                            onPressed: () {},
-//                            child: Row(
-//                              children: <Widget>[
-//                                Icon(
-//                                  Icons.done,
-//                                  color: isSelected
-//                                      ? Theme.of(context).primaryColorLight
-//                                      : Theme.of(context).primaryColorDark,
-//                                  size: Config.textSize(context, 3),
-//                                ),
-//                                SizedBox(
-//                                  width: Config.xMargin(context, 2),
-//                                ),
-//                                Text(
-//                                  'Done',
-//                                  style: TextStyle(
-//                                      fontWeight: FontWeight.bold,
-//                                      color: isSelected
-//                                          ? Theme.of(context).primaryColorLight
-//                                          : Theme.of(context).primaryColorDark),
-//                                )
-//                              ],
-//                            ),
-//                          )
-//                        ],
-//                      ),
-//                    )
-//                  ],
-//                )),
-//          ]),
-//    );
-//  }
-//}
 
 class MedicationCard extends StatefulWidget {
   final double height;
   final double width;
-  final String drugName;
-  final String drugType;
-  final String time;
-  final int dosage;
-  final String selectedFreq;
-  final MedicationReminder values;
-
-  MedicationCard(
-      {this.values,
-        this.drugName,
-        this.drugType,
-        this.time,
-        this.height,
-        this.width,
-        this.dosage,
-        this.selectedFreq});
-
+  MedicationCard({this.height, this.width});
   @override
   _MedicationCardState createState() => _MedicationCardState();
 }
 
 class _MedicationCardState extends State<MedicationCard> {
   bool isSelected = false;
-
   @override
   Widget build(BuildContext context) {
-    final medModel = Provider.of<MedicationData>(context);
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-//    print(height);
-//    print(width);
     return GestureDetector(
       //Navigate to screen with single reminder i.e the on user clicked on
       onTap: () {
@@ -234,23 +26,20 @@ class _MedicationCardState extends State<MedicationCard> {
           mainAxisSize: MainAxisSize.max,
           children: [
             Text(
-              medModel
-                  .convertTimeBack(widget.values.firstTime)
-                  .format(context)
-                  .toString(),
+              "8:00 AM",
             ),
-            SizedBox(height: height * 0.02),
+            SizedBox(height: widget.height * 0.02),
             Container(
-                width: width,
+                width: widget.width,
                 padding: EdgeInsets.symmetric(
                     horizontal: Config.xMargin(context, 3),
                     vertical: Config.yMargin(context, 1)),
                 decoration: BoxDecoration(
                   color: isSelected
                       ? Theme.of(context).primaryColor
-                      : Theme.of(context).backgroundColor,
+                      : Theme.of(context).primaryColorLight,
                   borderRadius:
-                  BorderRadius.circular(Config.xMargin(context, 5)),
+                      BorderRadius.circular(Config.xMargin(context, 5)),
                   boxShadow: [
                     BoxShadow(
                       color: Theme.of(context).primaryColorLight,
@@ -263,28 +52,25 @@ class _MedicationCardState extends State<MedicationCard> {
                     Row(
                       children: <Widget>[
                         Image.asset(
-                          widget.drugType,
+                          "images/injection.png",
 //                            color: Theme.of(context).primaryColorLight,
-                          width: width * 0.2,
-                          height: height * 0.1,
-                        ),
-                        SizedBox(
-                          width: Config.xMargin(context, 8.5),
+                          width: widget.width * 0.2,
+                          height: widget.height * 0.1,
                         ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Text(
-                              widget.values.drugName,
+                              'Chloroquine Injection',
                               style: TextStyle(
                                   color: isSelected
                                       ? Theme.of(context).primaryColorLight
                                       : Theme.of(context).primaryColorDark,
                                   fontWeight: FontWeight.bold),
                             ),
-                            SizedBox(height: height * 0.005),
+                            SizedBox(height: widget.height * 0.005),
                             Text(
-                              '${widget.dosage} - ${widget.selectedFreq} per day',
+                              '1 shots once daily',
                               style: TextStyle(
                                   color: isSelected
                                       ? Theme.of(context).primaryColorLight
@@ -300,7 +86,7 @@ class _MedicationCardState extends State<MedicationCard> {
                     ),
                     Divider(
                       color: Theme.of(context).primaryColorLight,
-                      height: height * 0.02,
+                      height: widget.height * 0.02,
 //indent: 50.0,
                       // endIndent: 10.0,
                     ),
@@ -311,43 +97,8 @@ class _MedicationCardState extends State<MedicationCard> {
                         children: <Widget>[
                           FlatButton(
                             onPressed: () {
-                              var medModel =
-                              Provider.of<MedicationData>(context);
-
-                              print(medModel
-                                  .updateDrugName(widget.values.drugName));
-                              print("id = " +
-                                  medModel.updateId(widget.values.id));
-                              print(
-                                  medModel.updateDosage(widget.values.dosage));
-                              medModel.updateStartDate(widget.values.startAt);
-                              medModel.updateEndDate(widget.values.endAt);
-                              print(
-                                  medModel.updateFreq(widget.values.frequency));
-
-                              if (medModel.selectedFreq == 'Once') {
-                                print(medModel.updateFirstTime(medModel
-                                    .convertTimeBack(widget.values.firstTime)));
-                              } else if (medModel.selectedFreq == 'Twice') {
-                                print(medModel.updateFirstTime(medModel
-                                    .convertTimeBack(widget.values.firstTime)));
-                                print(medModel.updateSecondTime(
-                                    medModel.convertTimeBack(
-                                        widget.values.secondTime)));
-                              } else if (medModel.selectedFreq == 'Thrice') {
-                                print(medModel.updateFirstTime(medModel
-                                    .convertTimeBack(widget.values.firstTime)));
-                                print(medModel.updateSecondTime(
-                                    medModel.convertTimeBack(
-                                        widget.values.secondTime)));
-                                print(medModel.updateThirdTime(medModel
-                                    .convertTimeBack(widget.values.thirdTime)));
-                              }
-
                               Navigator.pushNamed(
-                                context,
-                                RouteNames.medicationView,
-                              );
+                                  context, RouteNames.medicationView);
                             },
                             child: Text(
                               'View',
