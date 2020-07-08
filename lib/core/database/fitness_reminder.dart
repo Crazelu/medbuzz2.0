e4e498058bb5684d2fc52ea493aba49b9a7383dd
Gideon Peters <gideonpeters85@gmail.com>
2020-07-08 03:52:59 +0100

    chore: updated all fitness screen to fetch from db

--- lib/core/database/fitness_reminder.dart
+++ lib/core/database/fitness_reminder.dart
@@ -4,26 +4,27 @@ import '../models/fitness_reminder_model/fitness_reminder.dart';
 
 class FitnessReminderCRUD extends ChangeNotifier {
   static const String _boxName = "fitnessReminderBox";
-  List<FitnessReminder> fitnessReminder = [];
+  List<FitnessReminder> _fitnessReminder = [];
+  List<FitnessReminder> get fitnessReminder => _fitnessReminder;
 
   int get reminderLength {
-    return this.fitnessReminder.length;
+    return _fitnessReminder.length;
   }
 
   void getReminders() async {
     var box = await Hive.openBox<FitnessReminder>(_boxName);
-    fitnessReminder = box.values.toList();
+    _fitnessReminder = box.values.toList();
     notifyListeners();
   }
 
   getOneReminder(index) {
-    return fitnessReminder[index];
+    return _fitnessReminder[index];
   }
 
   void addReminder(FitnessReminder reminder) async {
     var box = Hive.box<FitnessReminder>(_boxName);
     await box.put(reminder.id, reminder);
-    this.fitnessReminder = box.values.toList();
+    _fitnessReminder = box.values.toList();
     box.close();
     notifyListeners();
   }
@@ -32,7 +33,7 @@ class FitnessReminderCRUD extends ChangeNotifier {
     int key = reminder.index;
     var box = Hive.box<FitnessReminder>(_boxName);
     await box.putAt(key, reminder);
-    this.fitnessReminder = box.values.toList();
+    _fitnessReminder = box.values.toList();
     box.close();
     notifyListeners();
   }
@@ -40,7 +41,7 @@ class FitnessReminderCRUD extends ChangeNotifier {
   void deleteReminder(FitnessReminder reminder) async {
     int key = reminder.index;
     var box = Hive.box<FitnessReminder>(_boxName);
-    this.fitnessReminder = box.values.toList();
+    _fitnessReminder = box.values.toList();
     await box.delete(key);
     box.close();
   }
