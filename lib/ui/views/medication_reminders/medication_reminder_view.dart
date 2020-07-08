66c8b4040a261c845b9f23076713e0165c9b6120
Chukwuebuka Onah <revelationjay02@gmail.com>
2020-07-08 11:49:09 +0100

    Fixed Bug: Correct drug type not showing in edit medication scree

--- lib/ui/views/medication_reminders/medication_reminder_view.dart
+++ lib/ui/views/medication_reminders/medication_reminder_view.dart
@@ -76,19 +76,7 @@ class MedicationView extends StatelessWidget {
                         Container(
                           padding: EdgeInsets.only(
                               right: Config.xMargin(context, 5)),
-                          child: Image.asset(medModel.selectedIndex == 0
-                              ? medModel.images[0]
-                              : medModel.selectedIndex == 1
-                                  ? medModel.images[1]
-                                  : medModel.selectedIndex == 2
-                                      ? medModel.images[2]
-                                      : medModel.selectedIndex == 3
-                                          ? medModel.images[3]
-                                          : medModel.selectedIndex == 4
-                                              ? medModel.images[4]
-                                              : medModel.selectedIndex == 5
-                                                  ? medModel.images[5]
-                                                  : medModel.images[6]),
+                          child: Image.asset(medModel.selectedDrugType),
                         ),
                       ],
                     ),
